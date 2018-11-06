6302d55ae1c1ca03cb4b79dd9d55f73c53f932a4
f6o <fluffy6h@gmail.com>
2018-11-06 11:43:48 +0000

    implementing my own cp

---
+++ cp1.c
@@ -0,0 +1,33 @@
+#include <utmp.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+error(const char * msg) {
+  fprintf(stderr, msg);
+  exit(1);
+}
+
+int
+main(int argc, char** argv)
+{
+  /* two file cp */
+  if ( argc != 3 ) {
+    error("two file copy only\n");
+  }
+
+  int src = open(argv[1], O_RDONLY);
+  if ( src == -1 )
+    error("could not open the source file.");
+  int dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, FILE_MODE);
+  if ( dst == -1 ) {
+    error("could not create the dest file.");
+  }
+
+  close(src);
+  close(dst);
+}
