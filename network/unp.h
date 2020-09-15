3c70b0c0db3be296b723ebb8850d6c70c8efab3e
f6o <fluffy6h@gmail.com>
2020-09-16 02:19:42 +0900

    implement simple daytime server

--- network/unp.h
+++ network/unp.h
@@ -17,10 +17,21 @@
 #include <strings.h>
 
 #define MAXLINE 4096
+#define LISTENQ 1024
 
 #define SA struct sockaddr
 
+/* error.c */
+
 void err_quit(const char *, ...);
 void err_sys(const char *, ...);
+
+/* wrap.c */
+int Accept(int, struct sockaddr *, socklen_t *);
+void Bind(int, struct sockaddr *, socklen_t);
+void Listen(int, int);
+int Socket(int, int, int);
+void Close(int);
+void Write(int, void *, size_t);
 
 #endif
